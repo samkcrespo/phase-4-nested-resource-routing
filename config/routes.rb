e74bedd786616047b31c345a2795fc3428e2f28d
Samantha Crespo <samkcrespo@gmail.com>
2022-06-07 22:09:36 -0700

    commit tree a42ebee5452208a292c49e5adbc78274a0482e7b

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :dog_houses, only: [:show]
+  resources :dog_houses, only: [:show] do
+    # nested resources for reviews of dog_house
+    resources :reviews, only: [:show, :index]
+  end
+  
   resources :reviews, only: [:show, :index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
